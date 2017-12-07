c32d49d9714b62feae069c09cc3dfeedbdce2da2
Viktor <vivat.bbb@gmail.com>
2017-12-08 00:48:57 +0200

    - work with number

--- js/index.js
+++ js/index.js
@@ -1,1 +1,23 @@
-console.log('javascript works');
+// принцип работы
+// - ловим значения, и записываем в строку.
+// - если всетретили равно, парсим строку
+var domCalc = document.querySelector('div.keys');
+domCalc.addEventListener('click',getNumber);
+
+var str = '';
+
+function getNumber(event) {
+    var evtar = event.target.getAttribute('data-value');
+    if (0<=evtar<=9){
+        // alert(evtar);
+        str += evtar;
+        var out = document.getElementById('input');
+        out.innerHTML = str;
+    }else if (event.target.className){
+        alert(event.target.className);
+    }
+}
+
+function calc() {
+
+}
